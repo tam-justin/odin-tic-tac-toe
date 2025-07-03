9c9375f32793b51b3b588623ce0c3bce3814216b
tam-justin <144107518+tam-justin@users.noreply.github.com>
2025-07-02 19:54:58 -0700

    files created

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const game = (function gameController(){
+    const board = new Array(9);
+})();
+
+const display = (function displayController(){
+
+})();
+
+function createPlayer(name, marker){
+    return {name, marker};
+}
